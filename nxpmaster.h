0d1fffcd88b5487168c48872a5c74f0154931eb8
Tobias Schaer <toby.schaer@gmail.com>
2012-01-04 18:02:41 -0500

    Initial commit

---
+++ nxpmaster.h
@@ -0,0 +1,18 @@
+// nxpmaster.h
+// 19.12.2011 T Schaer
+
+// ------ Application Settings
+// 1. Set Master node name
+#define ME "Master"
+// 2. Set cyclic program execution period
+#define CYCLETIME 500
+// 3. Set Slave node name
+#define SLAVE "Slave"
+
+// ------ Don't Touch
+#include "dictionary.nxc"
+#include "jsonparse.nxc"
+#include "jsonserialize.nxc"
+#include "common.nxc"
+#include "master.nxc"
+
---
+++ nxpslave.h
@@ -0,0 +1,19 @@
+// nxpslave.h
+// 19.12.2011 T Schaer
+
+// ------ Application Settings
+// 1. Set Slave node name
+#define ME "Slave"
+// 2. Set cyclic program execution period
+#define CYCLETIME 500
+// 3. Set Master node name
+#define MASTER "Master"
+
+// ------ Don't Touch
+#include "dictionary.nxc"
+#include "jsonparse.nxc"
+#include "jsonserialize.nxc"
+#include "common.nxc"
+#include "slave.nxc"
+
+
